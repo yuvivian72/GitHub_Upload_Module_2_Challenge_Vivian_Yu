ca2523418bffeccc43b6f2c75e5342d9a0af7ff0
yuvivian72 <107157533+yuvivian72@users.noreply.github.com>
2022-06-30 23:01:34 -0400

    Add files via upload

---
+++ loan_qualifier_app/qualifier/utils/fileio.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""Helper functions to load and save CSV data.
+
+This contains a helper function for loading and saving CSV files.
+
+"""
+import csv
+
+
+def load_csv(csvpath):
+    """Reads the CSV file from path provided.
+
+    Args:
+        csvpath (Path): The csv file path.
+
+    Returns:
+        A list of lists that contains the rows of data from the CSV file.
+
+    """
+    with open(csvpath, "r") as csvfile:
+        data = []
+        csvreader = csv.reader(csvfile, delimiter=",")
+
+        # Skip the CSV Header
+        next(csvreader)
+
+        # Read the CSV data
+        for row in csvreader:
+            data.append(row)
+    return data
+
+def save_csv(output_path, qualifying_loans):
+    header = ["Lender", "Max Loan Amount", "Max LTV", "Max DTI", "Min Credit Score", "Interest Rate"]
+    with open(output_path, 'w', newline = '') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(header)
